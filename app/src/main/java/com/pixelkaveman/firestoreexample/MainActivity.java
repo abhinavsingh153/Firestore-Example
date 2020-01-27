79d6df84beff2cda4847d85efd18fa77309362f4
abhinavsingh153 <abhinavsingh153@gmail.com>
2020-01-27 12:51:01 +0530

    Replace Hasmap with custom Java class and polish code

--- app/src/main/java/com/pixelkaveman/firestoreexample/MainActivity.java
+++ app/src/main/java/com/pixelkaveman/firestoreexample/MainActivity.java
@@ -17,6 +17,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.ListenerRegistration;
@@ -24,22 +25,24 @@ import com.google.firebase.firestore.ListenerRegistration;
 import java.util.HashMap;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    public static final String TAG = "MainActivity";
-    public static final String KEY_TITLE= "title";
-    public static final String KEY_DESCRIPTION = "description";
+    private static final String TAG = "MainActivity";
+
+    private static final String KEY_TITLE = "title";
+    private static final String KEY_DESCRIPTION = "description";
 
     private EditText editTextTitle;
     private EditText editTextDescription;
+    private TextView textViewData;
     private Button buttonSave;
     private Button buttonLoad;
-    private TextView textViewData;
+    private Button buttonUpdateDesc;
+    private Button buttonDeleteDesc;
+    private Button buttonDeleteNote;
 
-    // cretae a varibale which will be aused as a reference to our firestore database
-    FirebaseFirestore db = FirebaseFirestore.getInstance().getInstance();
-    DocumentReference noteRef = db.collection("Notebook").document("My first note");
-    private ListenerRegistration noteListener ;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private DocumentReference noteRef = db.document("Notebook/My First Note");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,121 +51,138 @@ public class MainActivity extends AppCompatActivity {
 
         editTextTitle = findViewById(R.id.edit_text_title);
         editTextDescription = findViewById(R.id.edit_text_description);
+        textViewData = findViewById(R.id.text_view_data);
         buttonSave = findViewById(R.id.save_btn);
         buttonLoad = findViewById(R.id.load_btn);
-        textViewData = findViewById(R.id.text_view_data);
+        buttonUpdateDesc = findViewById(R.id.update_description_btn);
+        buttonDeleteDesc = findViewById(R.id.delete_description_btn);
+        buttonDeleteNote = findViewById(R.id.delete_note_btn);
 
-        buttonSave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // create collection and save the data on firestore
-                saveNote();
-            }
-        });
-
-        //load button click
-        buttonLoad.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // load dtaata from firestore collections or documents
-
-                loadNote();
-            }
-        });
+        buttonSave.setOnClickListener(this);
+        buttonLoad.setOnClickListener(this);
+        buttonUpdateDesc.setOnClickListener(this);
+        buttonDeleteDesc.setOnClickListener(this);
+        buttonDeleteNote.setOnClickListener(this);
     }
-
 
     @Override
     protected void onStart() {
         super.onStart();
-
-        // listen to the changes in the firestore database in reltime.
-
-        noteListener =noteRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+        noteRef.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
             @Override
-            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
-
-                if(e != null){
-                    Toast.makeText(MainActivity.this, "Error while loading!" , Toast.LENGTH_LONG).show();
-                    Log.d(TAG , e.toString());
+            public void onEvent(DocumentSnapshot documentSnapshot, FirebaseFirestoreException e) {
+                if (e != null) {
+                    Toast.makeText(MainActivity.this, "Error while loading!", Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, e.toString());
                     return;
                 }
 
-                //Chaeck if the document snapshot exists
-                if(documentSnapshot.exists()){
-                    // poultae the textViewData with the title snd description data
-                    String title = documentSnapshot.getString(KEY_TITLE);
-                    String description = documentSnapshot.getString(KEY_DESCRIPTION);
+                if (documentSnapshot.exists()) {
+                    Note note = documentSnapshot.toObject(Note.class);
+
+                    String title = note.getTitle();
+                    String description = note.getDescription();
 
                     textViewData.setText("Title: " + title + "\n" + "Description: " + description);
-                    ;                        }
+                } else {
+                    textViewData.setText("");
+                }
             }
         });
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        // detaching the noteListener in on Stop
-        noteListener.remove();
-    }
+    public void saveNote() {
+        String title = editTextTitle.getText().toString();
+        String description = editTextDescription.getText().toString();
 
-    private void saveNote(){
+        Note note = new Note(title, description);
 
-        String title  = editTextTitle.getText().toString();
-        String description  = editTextDescription.getText().toString();
-
-        Map<String , Object> note = new HashMap<>();
-        note.put(KEY_TITLE , title);
-        note.put(KEY_DESCRIPTION , description);
-
-        //implicitly crete the collection and
-        //inside the clollection store a document
         noteRef.set(note)
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
-
-                        Toast.makeText(MainActivity.this, "Note saved" , Toast.LENGTH_LONG).show();
+                        Toast.makeText(MainActivity.this, "Note saved", Toast.LENGTH_SHORT).show();
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-
-                        Toast.makeText(MainActivity.this, "Error!" , Toast.LENGTH_LONG).show();
+                        Toast.makeText(MainActivity.this, "Error!", Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, e.toString());
                     }
                 });
-
     }
 
-    private void loadNote(){
+    public void updateDescription() {
+        String description = editTextDescription.getText().toString();
 
+        //Map<String, Object> note = new HashMap<>();
+        //note.put(KEY_DESCRIPTION, description);
 
+        //noteRef.set(note, SetOptions.merge());
+        noteRef.update(KEY_DESCRIPTION, description);
+    }
+
+    public void deleteDescription() {
+        //Map<String, Object> note = new HashMap<>();
+        //note.put(KEY_DESCRIPTION, FieldValue.delete());
+
+        //noteRef.update(note);
+        noteRef.update(KEY_DESCRIPTION, FieldValue.delete());
+    }
+
+    public void deleteNote() {
+        noteRef.delete();
+    }
+
+    public void loadNote() {
         noteRef.get()
                 .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
                     @Override
                     public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        if (documentSnapshot.exists()) {
+                            Note note = documentSnapshot.toObject(Note.class);
 
-                        //Chaeck if the document snapshot exists
-                        if(documentSnapshot.exists()){
-                            // poultae the textViewData with the title snd description data
-                            String title = documentSnapshot.getString(KEY_TITLE);
-                            String description = documentSnapshot.getString(KEY_DESCRIPTION);
+                            String title = note.getTitle();
+                            String description = note.getDescription();
 
                             textViewData.setText("Title: " + title + "\n" + "Description: " + description);
-;                        }
-                        else {
-                            Toast.makeText(MainActivity.this , "Documnet does'nt exist.",Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Document does not exist", Toast.LENGTH_SHORT).show();
                         }
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(MainActivity.this , "Error!",Toast.LENGTH_LONG).show();
-                        Log.d(TAG , e.toString());
+                        Toast.makeText(MainActivity.this, "Error!", Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, e.toString());
                     }
                 });
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+
+            case R.id.save_btn : saveNote();
+                break;
+
+            case R.id.load_btn : loadNote();
+                break;
+            case R.id.update_description_btn : updateDescription();
+                break;
+            case R.id.delete_description_btn : deleteDescription();
+                break;
+            case R.id.delete_note_btn : deleteNote();
+                break;
+
+                default: Log.d(TAG , v.getId()+"Button not found");
+
+
+
+
+        }
     }
 }
---
+++ app/src/main/java/com/pixelkaveman/firestoreexample/Note.java
@@ -0,0 +1,24 @@
+package com.pixelkaveman.firestoreexample;
+
+public class Note {
+
+        private String title;
+        private String description;
+
+        public Note() {
+            //public no-arg constructor needed
+        }
+
+        public Note(String title, String description) {
+            this.title = title;
+            this.description = description;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+}
